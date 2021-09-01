79c7ed9f10b3a76c0602aab993c6d81c7fa7afb8
rippoukotsu <89211905+rippoukotsu@users.noreply.github.com>
2021-09-01 21:20:55 +0900

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,10 +1,17 @@
 
-$(".qa-list dd").hide();
-$(".qa-list dl").on("click", function(e){
-    $('dd',this).slideToggle('fast');
-    if($(this).hasClass('open')){
-        $(this).removeClass('open');
-    }else{
-        $(this).addClass('open');
-    }
+$(function() {
+  $('.btn-gNav').on("click", function(){
+
+    $(this).toggleClass('open');
+    $('#gNav').toggleClass('open');
+  });
+
+});
+
+// メニューをクリックされたら、非表示にする
+$(function() {
+  $('.gNav-menu li a').on("click", function(){
+     $('#gNav').removeClass('open');
+     $('.btn-gNav').removeClass('open');
+  });
 });
