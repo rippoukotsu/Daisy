c8e14c9a02e279ee3ec674faf094f59e43527712
rippoukotsu <89211905+rippoukotsu@users.noreply.github.com>
2021-09-08 00:12:26 +0900

    Add files via upload

--- faq.js
+++ faq.js
@@ -1,13 +1,13 @@
 
-// $(".qa-list dd").hide();
-// $(".qa-list dl").on("click", function(e){
-//     $('dd',this).slideToggle('fast');
-//     if($(this).hasClass('open')){
-//         $(this).removeClass('open');
-//     }else{
-//         $(this).addClass('open');
-//     }
-// });
+$(".qa-list dd").hide();
+$(".qa-list dl").on("click", function(e){
+    $('dd',this).slideToggle('fast');
+    if($(this).hasClass('open')){
+        $(this).removeClass('open');
+    }else{
+        $(this).addClass('open');
+    }
+});
 
 
 
