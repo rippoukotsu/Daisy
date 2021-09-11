0da6cd54b11f22d6d9a165efff3abdad2d102185
rippoukotsu <89211905+rippoukotsu@users.noreply.github.com>
2021-09-12 01:56:19 +0900

    Add files via upload

--- faq.js
+++ faq.js
@@ -1,4 +1,5 @@
 
+
 $(".qa-list dd").hide();
 $(".qa-list dl").on("click", function(e){
     $('dd',this).slideToggle('fast');
@@ -43,7 +44,7 @@ $(function() {
   $(window).scroll(function (){
     //ふわっと動くきっかけのクラス名と動きのクラス名の設定
 $('.lineTrigger').each(function(){ //fadeInUpTriggerというクラス名が
-　　var elemPos = $(this).offset().top-50; //要素より、50px上の
+　　var elemPos = $(this).offset().top+50; //要素より、50px上の
 　　var scroll = $(window).scrollTop();
 　　var windowHeight = $(window).height();
 　　if (scroll >= elemPos - windowHeight){
@@ -75,6 +76,63 @@ $(this).removeClass('rline');
 　　// 画面外に出たらfadeInというクラス名を外す
 　　}
 　　});
+
+
+$('.flineTrigger').each(function(){
+var elemPos = $(this).offset().top-50; //要素より、50px上の
+　　var scroll = $(window).scrollTop();
+　　var windowHeight = $(window).height();
+　　if (scroll >= elemPos - windowHeight){
+　
+$(this).addClass('flipRightTop');
+　$(this).addClass('fline');
+　　// 画面内に入ったらfadeInというクラス名を追記
+　　}else{
+　　　
+　$(this).removeClass('flipRightTop');
+$(this).removeClass('fline');
+　　// 画面外に出たらfadeInというクラス名を外す
+　　}
+　　});
+
+
+$('.clineTrigger').each(function(){
+var elemPos = $(this).offset().top-50; //要素より、50px上の
+　　var scroll = $(window).scrollTop();
+　　var windowHeight = $(window).height();
+　　if (scroll >= elemPos - windowHeight){
+　
+$(this).addClass('rflipRightTop');
+　$(this).addClass('cline');
+　　// 画面内に入ったらfadeInというクラス名を追記
+　　}else{
+　　　
+　$(this).removeClass('rflipRightTop');
+$(this).removeClass('cline');
+　　// 画面外に出たらfadeInというクラス名を外す
+　　}
+　　});
+
+
+
+
+$('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+　　var elemPos = $(this).offset().top-100; //要素より、50px上の
+　　var scroll = $(window).scrollTop();
+　　var windowHeight = $(window).height();
+　　if (scroll >= elemPos - windowHeight){
+　
+$(this).addClass('fadeUp');
+
+　　// 画面内に入ったらfadeInというクラス名を追記
+　　}else{
+　　　
+
+$(this).removeClass('fadeUp');
+　　// 画面外に出たらfadeInというクラス名を外す
+　　}
+　　});
+
 
 
   });// ここまで画面をスクロールをしたら動かしたい場合の記述
