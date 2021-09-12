1d6c6f685bdfdb40e1bbf52a2424c7fdaa5e2ec9
rippoukotsu <89211905+rippoukotsu@users.noreply.github.com>
2021-09-12 13:34:20 +0900

    Add files via upload

--- faq.js
+++ faq.js
@@ -44,7 +44,7 @@ $(function() {
   $(window).scroll(function (){
     //ふわっと動くきっかけのクラス名と動きのクラス名の設定
 $('.lineTrigger').each(function(){ //fadeInUpTriggerというクラス名が
-　　var elemPos = $(this).offset().top+50; //要素より、50px上の
+　　var elemPos = $(this).offset().top+200; //要素より、50px上の
 　　var scroll = $(window).scrollTop();
 　　var windowHeight = $(window).height();
 　　if (scroll >= elemPos - windowHeight){
@@ -134,6 +134,37 @@ $(this).removeClass('fadeUp');
 　　});
 
 
+$('.fadeDownTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+　　var elemPos = $(this).offset().top-100; //要素より、50px上の
+　　var scroll = $(window).scrollTop();
+　　var windowHeight = $(window).height();
+　　if (scroll >= elemPos - windowHeight){
+　
+$(this).addClass('fadeDown');
+
+　　// 画面内に入ったらfadeInというクラス名を追記
+　　}else{
+　　　
+
+$(this).removeClass('fadeDown');
+　　// 画面外に出たらfadeInというクラス名を外す
+　　}
+　　});
+
+
 
   });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
+
+
+
+ $(function () {
+            $('a[href^="#"]').click(function () {
+                var time = 500;
+                var href = $(this).attr("href");
+                var target = $(href == "#" || href == "" ? 'html' : href);
+                var position = target.offset().top;
+                $("html,body").animate({ scrollTop: position }, time, "swing");
+                return false;
+            });
+        });
